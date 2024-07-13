f28fd2f60df6ee1274f3164198ac1967f5c64066
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-13 16:33:41 +0200

    Create Employee Entity

---
+++ src/main/java/com/dgmf/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.dgmf.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Document(collection = "employees")
+public class Employee {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
